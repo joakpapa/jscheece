f6c26ef7a5a3de18185df169838d4ada53336672
Joakim Papadopoulos <77551838+joakpapa@users.noreply.github.com>
2024-07-21 11:26:33 +0300

    Add files via upload

---
+++ customerAccount-page.php
@@ -0,0 +1,244 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION["customer_email"])) {
+
+    header("Location: login-page.php");
+} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JSCheese</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="style.css" type="text/css" rel="stylesheet"/>   
+</head>
+<body>
+<!--Αρχή header-->
+<div class="container-fluid">
+    <h1>
+        <a class="navbar-brand" href="index.php">
+        <img src="jscheeselogo.png" width="80" height="80" class="d-inline-block align-top" alt="JSCheese Logo">
+    </a>
+        JSCheese - Το Τυροκομείο της Καβάλας
+    </h1>
+    <!--Αρχή nav-->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Η Εταιρεία</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.html">Προϊόντα</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            Υπηρεσίες
+                        </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="orderscunstomer.php">Διαχειριστής</a></li>
+                        <li><a class="dropdown-item" href="geusignosiacalender.html">Εκδηλώσεις</a></li>
+                        <li><a class="dropdown-item" href="pelates.html">Οι Πελάτες μας</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="login-page.php">Λογαριασμός</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="epikinonia.html">Επικοινωνία</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>    
+<!--Τέλος nav-->
+<!--Τέλος header-->
+
+<!-- Ο Λογαριασμός του Πελάτη -->
+<h3 class="d-flex justify-content-center pt-5">
+    <?php echo "Καλώς ήρθατε " . $_SESSION['customer_company'];?>
+</h3>
+<!--Αρχή εικόνας για αποσύνδεση-->
+<a id="logoutLink" class="d-flex justify-content-center" href="procedures/logout.php"> </a>
+    <br>
+
+    <script>
+        var logoutLink = document.getElementById('logoutLink');
+
+        var logoutIcon = document.createElement('img');
+        logoutIcon.src = 'img/logout.png'; 
+
+        logoutLink.innerHTML = '';
+        logoutLink.appendChild(logoutIcon); 
+    </script>
+    <!--Τέλος εικόνας για αποσύνδεση-->
+    
+<div class="container-fluid d-flex justify-content-center">
+    <div class="col-md-5 mb-5">
+			<h4>Στοιχεία Λογαριασμού</h4>
+			<hr>
+			<!--- Φόρμα - Στοιχεία Πελάτη --->
+			<form action="procedures/update_customer.php" method="post">
+				<div class="row">
+					<div class="col-md-6 mb-3">
+						<label for="fname">Όνομα*</label>
+						<input type="text" class="form-control" name="customer_fname" value="<?php echo $_SESSION['customer_fname'];?>" required>
+					</div>
+					<div class="col-md-6 mb-3">
+						<label for="lname">Επώνυμο*</label>
+						<input type="text" class="form-control" name="customer_lname" value="<?php echo $_SESSION['customer_lname'];?>" required>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 mb-3">
+						<label for="email">Email*</label>
+						<input type="text" class="form-control" name="customer_email" 
+                            value="<?php echo $_SESSION['customer_email'];?>" required
+                        >
+					</div>
+					<div class="col-md-6 mb-3">
+						<label for="password">Κωδικός*</label>
+						<input type="text" class="form-control" name="customer_password" 
+                            value="<?php echo $_SESSION['customer_password'];?>" required
+                        >
+					</div>	
+				</div>
+				<div class="row">
+					<div class="col-md-6 mb-3">
+						<label for="tel">Κινητό</label>
+						<input type="text" class="form-control" name="customer_phone" 
+                            value="<?php echo $_SESSION['customer_phone'];?>" 
+                        >
+					</div>
+					<div class="col-md-6 mb-3">
+						<label for="company">Εταιρία:</label>
+						<input type="text" class="form-control" name="customer_company" 
+                            value="<?php echo $_SESSION['customer_company'];?>" required
+                        >
+					</div>
+				</div>				
+				<div class="row">
+					<div class="col-md-12 mb-3">
+						<label for="address">Διεύθυνση</label>
+						<input type="text" class="form-control" name="customer_address"
+                            value="<?php echo $_SESSION['customer_address']; ?>" required
+                        >
+					</div>
+				</div>
+                <!--Αρχή εικόνας Αποθήκευση και Διαγραφή-->				
+				<div class="d-grid gap-2">
+        <button id="saveBtn" class="btn btn-success" type="submit">
+        </button>
+
+        <a id="deleteLink" class="btn btn-danger" href="procedures/delete_customer.php">
+        </a>
+    </div>
+
+    <script>
+
+        var saveBtn = document.getElementById('saveBtn');
+        var saveIcon = document.createElement('img');
+        saveIcon.src = 'img/save.png';
+        saveBtn.innerHTML = ''; 
+        saveBtn.appendChild(saveIcon); 
+
+        var deleteLink = document.getElementById('deleteLink');
+        var deleteIcon = document.createElement('img');
+        deleteIcon.src = 'img/delete.png'; 
+        deleteLink.innerHTML = '';
+        deleteLink.appendChild(deleteIcon);
+    </script>	
+            <!--Τέλος εικόνας Αποθήκευση και Διαγραφή-->			
+			</form>	
+	</div> 
+</div>
+
+<!--Αρχή footer-->
+<footer class="bg-dark text-white">    
+    <div class="container-fluid text-center text-md-left pt-5 pb-4 mx-auto">
+        <div class="row text-center text-md-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3"> 
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">JSCheese</h5>
+                <p>Το Τυροκομείο στην Καβάλα που φέρνει τη γεύση στη ζωή σας.</p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Προϊόντα</h5>
+                <p><a href="products.html">Φρέσκα Τυροκομικά</a></p>
+                <p><a href="pelates.html">Οι Πελάτες μας</a></p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Υπηρεσίες</h5>
+                <p><a href="geusignosiacalender.html">Εκδηλώσεις</a></p>
+                <p><a href="login-page.php">Λογαριασμός</a></p>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Επικοινωνία</h5>
+                <div class="contSR">Διεύθυνση: Σάμου 7, 65201 Καβάλα</div>
+                <div class="contSR"><span>E-mail:</span> <a href="mailto:info@jscheese.gr">info@jscheese.gr</a></div>
+                <div class="contSR"><span>Τηλ.:</span> <a href="tel:+302510123456">+30 2510 123456</a></div>
+            </div>
+            </div>
+            <hr class="mb-4">
+            <div class="row align-items-center">
+                <div class="col-md-7 col-lg-8">
+                    <p>© Copyright 2024 JSCheese | Web Design &amp; Development by joakpap & nisonis</p>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;">
+                                    <i class="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;">
+                                <i class="fab fa-facebook"></i>
+                            </a>                  
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="https://www.facebook.com/" class="btn-floating btn-sm text-white" style="font-size:23px;">
+                                <i class="fab fa-facebook"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-facebook"  type="button" aria-current="page" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                </svg>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.youtube.com/" class="btn-floating btn-sm text-white" style="font-size:23px;">
+                                <i class="fab fa-youtube"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+                                <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                                </svg>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.instagram.com/" class="btn-floating btn-sm text-white" style="font-size:23px;">
+                                <i class="fab fa-instagram"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                                </svg>
+                                </a>
+                            </li>			  	 			  				  					  	 
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--Τέλος footer-->
+</body>
+</html>
---
+++ ex3.php
@@ -0,0 +1,37 @@
+﻿<html> 
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-7">
+</head>
+<body>
+<?php 
+$conn = mysqli_connect('localhost','nisonis','CaPYwq2p');
+mysqli_select_db($conn,'nisonis');
+mysqli_query($conn,'create table adminorder (Παραγγελία int not null auto_increment, Πελάτης varchar(100) not null,Κωδικός_Προϊόντος varchar(100),Ποσότητα varchar(100),primary key(Παραγγελία))');
+mysqli_query($conn,'insert into adminorder (Πελάτης,Κωδικός_Προϊόντος,Ποσότητα) values ("ΑΒ Βασιλόπουλος","14563","10"),("Carrefour Μαρινόπουλος","34987","32"),("CiTY market","36289","120"),("Eλληνικά market","48721","234")');
+$y=mysqli_query($conn,"show tables");
+echo 'TABLES<br>';
+echo '-----------<br>';
+while ($x=mysqli_fetch_row($y))
+{
+  echo $x[0].'<br>';
+}
+
+echo '<br><br>TABLE adminorder<br>';
+echo '------------------------<br>';
+$y=mysqli_query($conn,"describe adminorder");
+while ($x=mysqli_fetch_row($y))
+{
+  echo $x[0].' '.$x[1].' '.$x[2].' '.$x[3].' '.$x[4].'<br>';
+}
+
+echo '<br><br>TABLE adminorder RECORDS<br>';
+echo '-----------------------------------<br>';
+$y=mysqli_query($conn,"select * from adminorder");
+while ($x=mysqli_fetch_array($y))
+{
+  echo $x['Παραγγελία'].' '.$x['Πελάτης'].' '.$x['Κωδικός_Προϊόντος'].' '.$x['Ποσότητα'].'<br>';
+}
+mysqli_close(); 
+?>
+</body>
+</html>
